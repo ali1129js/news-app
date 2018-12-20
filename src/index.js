90f42ff3b3a70b6b6e183ea3dfb6d716469ace9b
ali1129js <ali1129js@gmail.com>
2018-12-20 15:34:30 +0100

    clean up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+/**
+ * @Author: Ali
+ * @Date:   2018-12-20T15:33:06+01:00
+ * @Last modified by:   Ali
+ * @Last modified time: 2018-12-20T15:33:06+01:00
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
