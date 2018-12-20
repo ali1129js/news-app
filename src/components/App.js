ae1c19be0f92d7d24c6a68bb4497b005889553e8
ali1129js <ali1129js@gmail.com>
2018-12-20 16:47:57 +0100

    fetch works

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@
  * @Author: Ali
  * @Date:   2018-12-20T15:33:39+01:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-12-20T15:42:07+01:00
+ * @Last modified time: 2018-12-20T16:36:07+01:00
  */
 
 import React, { Component } from 'react';
@@ -12,9 +12,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-        <h1 className="App-title"> My Feed</h1>
-        </header>
+      <header className="App-header">
+      <h1 className="App-title"> My Feed</h1>
+      </header>
         <News />
       </div>
     );
--- src/components/News/News.js
+++ src/components/News/News.js
@@ -2,14 +2,33 @@
  * @Author: Ali
  * @Date:   2018-12-20T15:41:20+01:00
  * @Last modified by:   Ali
- * @Last modified time: 2018-12-20T15:54:14+01:00
+ * @Last modified time: 2018-12-20T16:35:58+01:00
  */
 import React, { Component } from 'react'
 import NewSingle from './NewSingle'
 
 class News extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      news: []
+    }
+  }
+  componentDidMount() {
+    const url = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=063bd9bcd1b847f9a6146f60d88e2808'
+    fetch(url)
+    .then((res) => {
+      return res.json()
+    })
+    .then((data) => {
+      this.setState({
+        news:data.articles
+      })
+    })
+    .catch((error) => console.log(error))
+  }
   renderItem() {
-    return this.props.items.map((item) => (
+    return this.state.news.map((item) => (
       <NewSingle key={item.id} item={item} />
     ))
   }
