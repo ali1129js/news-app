0b849231756420de6819081b18d986e043e2602b
ali1129js <ali1129js@gmail.com>
2019-01-12 03:42:33 +0100

    Updates

---
+++ precache-manifest.504b25d95b146cc44afb9fa68fa17474.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd5b3b6b432921443a6d",
+    "url": "/news-app/static/js/runtime~main.dd5b3b6b.js"
+  },
+  {
+    "revision": "e04d50f68bbbcbf56fa3",
+    "url": "/news-app/static/js/main.e04d50f6.chunk.js"
+  },
+  {
+    "revision": "7e74f70ca4539de25dc9",
+    "url": "/news-app/static/js/1.7e74f70c.chunk.js"
+  },
+  {
+    "revision": "e04d50f68bbbcbf56fa3",
+    "url": "/news-app/static/css/main.b9b5f969.chunk.css"
+  },
+  {
+    "revision": "7e74f70ca4539de25dc9",
+    "url": "/news-app/static/css/1.4adc31a5.chunk.css"
+  },
+  {
+    "revision": "f69132044896482c5f1a02f10ee3decf",
+    "url": "/news-app/index.html"
+  }
+];
