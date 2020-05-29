da8000e937f51444f2302bcd944a7668d06ef810
ali1129js <ali1129js@gmail.com>
2020-05-29 12:50:01 +0200

    grid and refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,13 @@
  * @Author: Ali
  * @Date:   2018-12-20T15:33:39+01:00
  * @Last modified by:   Ali
- * @Last modified time: 2020-05-27T17:47:24+02:00
+ * @Last modified time: 2020-05-29T12:49:20+02:00
  */
 
 import React, { Component, Fragment } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import News from "./News/News";
-import SideNews from "./News/SideNews";
+
+import Grid from "./Grid";
 import Nav from "./Nav";
 
 class App extends Component {
@@ -40,33 +40,16 @@ class App extends Component {
           <Switch>
             <Fragment>
               <div className="container-fluid">
-                <Nav changeSource={this.changeSource} />
-                <div className="row">
-                  <div className="col-sm-8">
-                    <Route
-                      exact
-                      path="/"
-                      render={() => (
-                        <News
-                          newSource={this.state.newSource}
-                          sourceName={this.state.sourceName}
-                        />
-                      )}
-                    />
-                  </div>
-                  <div className="col-sm-4">
-                    <Route
-                      exact
-                      path="/"
-                      render={() => (
-                        <SideNews
-                          newCountry={this.state.newCountry}
-                          country={this.state.country}
-                        />
-                      )}
-                    />
-                  </div>
-                </div>
+                <Nav
+                  changeSource={this.changeSource}
+                  changeCountry={this.changeCountry}
+                />
+                <Grid
+                  newSource={this.state.newSource}
+                  sourceName={this.state.sourceName}
+                  newCountry={this.state.newCountry}
+                  country={this.state.country}
+                />
               </div>
             </Fragment>
           </Switch>
