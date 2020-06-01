3fc6e915a570c2c3097c3df2f50bd70ae31a2a9d
ali1129js <ali1129js@gmail.com>
2020-06-01 14:17:24 +0200

    new navbar works

--- src/components/TagNav.js
+++ src/components/TagNav.js
@@ -2,62 +2,74 @@
  * @Author: Ali
  * @Date:   2020-05-29T12:53:00+02:00
  * @Last modified by:   Ali
- * @Last modified time: 2020-05-30T12:45:31+02:00
+ * @Last modified time: 2020-06-01T14:16:15+02:00
  */
-import React from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import NewSingle from "./News/NewSingle";
 
 const TagNav = props => {
+  const [news, setNews] = useState([]);
+  const [source, setSource] = useState("");
+
   const handleclick = e => {
     let value = e.target.value;
-    console.log(value);
-    //test
-    const url = `https://newsapi.org/v2/top-headlines?country=${value}&apiKey=${
-      process.env.REACT_APP_API_KEY
-    }`;
-    axios
-      .get(url)
-      .then(res => {
-        console.log(res.data.articles);
-      })
-      .catch(error => console.log(error));
+    setSource(value);
+    renderItem();
   };
+  const renderItem = () => {
+    if (news.length === 0) {
+      console.log(news);
+    } else {
+      return news.data.map(item => <NewSingle key={item.title} item={item} />);
+    }
+  };
+  useEffect(
+    () => {
+      const url = `https://newsapi.org/v2/everything?q=${source}&apiKey=${
+        process.env.REACT_APP_API_KEY
+      }`;
+      axios
+        .get(url)
+        .then(res => {
+          setNews({ data: res.data.articles });
+        })
+        .catch(error => console.log(error));
+    },
+    [source]
+  );
   return (
     <div className="newNavBar">
       <button
         onClick={handleclick}
-        value={"us"}
-        className="btn btn-outline-danger"
+        value={"apple"}
+        className="btn btn-outline-danger m-2"
       >
-        handleclick
+        Apple News
       </button>
-      <button onClick={props.changeSource} className="btn btn-outline-danger">
-        Danger
+      <button
+        onClick={handleclick}
+        value={"amd"}
+        className="btn btn-outline-danger m-2"
+      >
+        AMD News
       </button>
-      <button onClick={props.changeSource} className="btn btn-outline-danger">
-        Danger
+      <button
+        onClick={handleclick}
+        value={"intel"}
+        className="btn btn-outline-danger m-2"
+      >
+        Intel News
       </button>
-      <button onClick={props.changeSource} className="btn btn-outline-danger">
-        Danger
+      <button
+        onClick={handleclick}
+        value={"bitcoin"}
+        className="btn btn-outline-danger m-2"
+      >
+        Bitcoin News
       </button>
-      <button onClick={props.changeSource} className="btn btn-outline-danger">
-        Danger
-      </button>
-      <button onClick={props.changeSource} className="btn btn-outline-danger">
-        Danger
-      </button>
-      <button onClick={props.changeSource} className="btn btn-outline-danger">
-        Danger
-      </button>
-      <button onClick={props.changeSource} className="btn btn-outline-danger">
-        Danger
-      </button>
-      <button onClick={props.changeSource} className="btn btn-outline-danger">
-        Danger
-      </button>
-      <button onClick={props.changeSource} className="btn btn-outline-danger">
-        Danger
-      </button>
+
+      <div className="row">{source && renderItem()}</div>
     </div>
   );
 };
